f94d406f33690af34d4e11fcd5d17f2cd49edce6
Derek Smith <dcsmith1212@gmail.com>
2020-05-11 14:21:21 -0600

    Add new GUI style and random wavelet visualizer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const masterPanCtrl = document.querySelector('#master-pan');
 const playPauseBtn = document.querySelector('#play-pause');
 const waveletSpacingSldr = document.querySelector('#wavelet-sldr');
 const waveletLengthSldr = document.querySelector('#wavelet-len-sldr');
+const waveletVisual = document.querySelector('.wavelet-vis');
+const visualContainer = document.querySelector('.visual');
+const panUniformRadio = document.getElementById('uniform');
+const panNormalRadio = document.getElementById('normal');
+
+
 const activeKeys = {};
 
 let isPlaying = false;
@@ -16,6 +22,25 @@ const pannerOpts = { pan: 0 };
 const masterPanNode = new StereoPannerNode(audioCtx, pannerOpts);
 masterGainNode.connect(masterPanNode);
 masterPanNode.connect(audioCtx.destination)
+
+const visualWidth = 500;
+const visualHeight = 200;
+visualContainer.style.width = visualWidth;
+visualContainer.style.height = visualHeight;
+
+let panDistribution = 'uniform';
+
+// Standard Normal variate using Box-Muller transform.
+function normal(mean, standardDeviation) {
+    let u, v, s;
+    do {
+        u = 2 * Math.random() - 1;
+        v = 1 * Math.random() - 1;
+        s = u * u + v * v;
+    } while (s >= 1);
+    if (s === 0) return 0;
+    return mean + standardDeviation * u * Math.sqrt(-2 * Math.log(s) / s);
+}
 
 function createTrack(note, shape = 'sine') {
     const oscillator = audioCtx.createOscillator();
@@ -31,6 +56,35 @@ function createTrack(note, shape = 'sine') {
 
     return { oscillator, gainNode, panNode };
 }
+
+function generateRandomVisual(lengthSldrVal, waveletSpacing) {
+    const waveletWidth = visualWidth * (10 * lengthSldrVal + 1) / 100;
+    const minTime = waveletWidth / 2;
+    const maxTime = visualWidth - minTime;
+    visualContainer.innerHTML = '';
+
+    const numWavelets = Math.ceil(360 * (0.5001 - waveletSpacing) + 20);
+    for (let i = 0; i < numWavelets; i++) {
+        const time = Math.round((visualWidth - waveletWidth) * Math.random());
+        let pan;
+        if (panDistribution === 'uniform') {
+            pan = Math.round((visualHeight - 2) * Math.random());
+        } else if (panDistribution === 'normal') {
+            pan = Math.round((visualHeight - 2) * normal(0.5, 1 / 6));
+        }
+
+        if (0 <= pan && pan <= visualHeight) {
+            const wavelet = document.createElement('div');
+            wavelet.style.top = `${pan}px`;
+            wavelet.style.left = `${time}px`;
+            wavelet.style.width = `${waveletWidth}px`;
+            wavelet.classList.add('wavelet-vis');
+            visualContainer.appendChild(wavelet);
+        }
+    }
+}
+
+generateRandomVisual(waveletLengthSldr.value, waveletSpacingSldr.value);
 
 // Master controls 
 masterGainCtrl.addEventListener('input', function () {
@@ -55,15 +109,26 @@ masterPanCtrl.addEventListener('input', function () {
 let waveletDuration = 0.2;
 waveletLengthSldr.addEventListener('input', function () {
     waveletDuration = parseFloat(this.value);
+    generateRandomVisual(this.value, waveletSpacingSldr.value);
 })
 
 let meanWaitTime = 0.2;
 waveletSpacingSldr.addEventListener('input', function () {
     meanWaitTime = parseFloat(this.value);
+    console.log(waveletLengthSldr);
+    generateRandomVisual(waveletLengthSldr.value, this.value);
 })
 
 function applyEnvelope(track) {
-    track.panNode.pan.value = 2 * Math.random() - 1;
+    if (panDistribution === 'uniform') {
+        track.panNode.pan.value = 2 * Math.random() - 1;
+    } else if (panDistribution === 'normal') {
+        let panValue;
+        do {
+            panValue = normal(0, 0.25);
+        } while (panValue > 1 || panValue < -1);
+        track.panNode.pan.value = panValue;
+    }
     track.gainNode.gain.cancelScheduledValues(audioCtx.currentTime);
     track.gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
     track.gainNode.gain.linearRampToValueAtTime(masterGainCtrl.value, audioCtx.currentTime + (waveletDuration / 2));
@@ -129,3 +194,13 @@ window.addEventListener('click', e => {
         playPauseBtn.textContent = isPlaying ? 'Pause' : 'Play';
     }
 });
+
+
+function updatePanDistribution(e) {
+    panDistribution = e.target.value;
+    generateRandomVisual(waveletLengthSldr.value, waveletSpacingSldr.value);
+
+}
+
+panUniformRadio.addEventListener('click', updatePanDistribution);
+panNormalRadio.addEventListener('click', updatePanDistribution);
